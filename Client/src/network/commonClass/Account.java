4b8dc45cb6d936bf824a0b98ca7e8ae4c127eef9
ZiQin <ZiQin9510@163.com>
2018-06-18 15:14:11 +0800

    【增加类】AccoutBuilder构造器 【增加功能】增加Account类成员变量 【增加功能】MessageOperate增加打包和解包个人详细信息方法

--- Client/src/network/commonClass/Account.java
+++ Client/src/network/commonClass/Account.java
@@ -3,7 +3,7 @@ package network.commonClass;
 /**
  * 描述：Account类是用户信息的封装，继承AccountBase类
  * @author 土豆
- * @version 1.1.0
+ * @version 1.2.0
  */
 public class Account extends AccountBase{
 	/**
@@ -23,6 +23,30 @@ public class Account extends AccountBase{
 	 */
 	protected Picture picture;
 	/**
+	 * 描述：手机号码
+	 */
+	protected String mobliePhone;
+	/**
+	 * 描述：个人邮箱
+	 */
+	protected String mail;
+	/**
+	 * 描述：等级
+	 */
+	protected byte stage;
+	/**
+	 * 描述：年龄
+	 */
+	protected int old;
+	/**
+	 * 描述：性别
+	 */
+	protected boolean sex;
+	/**
+	 * 描述：归属地
+	 */
+	protected String home;
+	/**
 	 * 描述：构造函数
 	 */
 	public Account(){
@@ -31,6 +55,12 @@ public class Account extends AccountBase{
 		online = false;
 		signature = new String();
 		picture = null;
+		stage = 0;
+		old = 0;
+		sex = false;
+		home = null;
+		mobliePhone = null;
+		mail = null;
 	}
 	/**
 	 * 描述：构造函数
@@ -45,6 +75,13 @@ public class Account extends AccountBase{
 		this.online = online;
 		this.signature = new String(signature);
 		this.picture = null;
+
+		stage = 0;
+		old = 0;
+		sex = false;
+		home = null;
+		mobliePhone = null;
+		mail = null;
 	}
 	/**
 	 * 描述：构造函数
@@ -55,6 +92,13 @@ public class Account extends AccountBase{
 		this.online = online;
 		this.signature = new String(signature);
 		this.picture = new Picture(picture);
+
+		stage = 0;
+		old = 0;
+		sex = false;
+		home = null;
+		mobliePhone = null;
+		mail = null;
 	}
 	/**
 	 * 描述：构造函数
@@ -65,7 +109,52 @@ public class Account extends AccountBase{
 		this.online = online;
 		this.signature = new String(signature);
 		this.picture = picture;
+
+		stage = 0;
+		old = 0;
+		sex = false;
+		home = null;
+		mobliePhone = null;
+		mail = null;
 	}
+
+	/**
+	 * 描述：构造函数
+	 * @param id
+	 * @param name
+	 * @param mobliePhone
+	 * @param mail
+	 * @param stage
+	 * @param old
+	 * @param sex
+	 * @param home
+	 * @param signature
+	 * @param isOnline
+	 * @param picture
+	 */
+	public Account(String id,
+                   String name,
+                   String mobliePhone,
+                   String mail,
+                   byte stage,
+                   int old,
+                   boolean sex,
+                   String home,
+                   String signature,
+                   boolean isOnline,
+				   Picture picture) {
+	    super.setId(id);
+	    this.name = name;
+	    this.mobliePhone = mobliePhone;
+	    this.mail = mail;
+	    this.stage = stage;
+	    this.old = old;
+	    this.sex = sex;
+	    this.home = home;
+	    this.signature = signature;
+	    this.online = isOnline;
+	    this.picture = picture;
+    }
 
 	/**
 	 * 描述：获取ID
@@ -130,4 +219,100 @@ public class Account extends AccountBase{
 	public void setPicture(byte[] pictureStream) {
 		this.picture = new Picture(pictureStream);
 	}
+
+    /**
+     * 描述：获取手机号码
+     * @return 手机号码
+     */
+	public String getMobliePhone() {
+	    return mobliePhone;
+    }
+
+    /**
+     * 描述：设置手机号码
+     * @param mobliePhone 手机号码
+     */
+    public void setMobliePhone(String mobliePhone) {
+	    this.mobliePhone = mobliePhone;
+    }
+
+    /**
+     * 描述：获取邮箱
+     * @return 邮箱地址
+     */
+    public String getMail() {
+	    return mail;
+    }
+
+    /**
+     * 设置邮箱
+     * @param mail 邮箱地址
+     */
+    public void setMail(String mail) {
+	    this.mail = mail;
+    }
+
+    /**
+     * 描述：获取等级
+     * @return 等级
+     */
+    public byte getStage() {
+	    return stage;
+    }
+
+    /**
+     * 描述：设置等级
+     * @param stage 等级
+     */
+    public void setStage(byte stage) {
+	    this.stage = stage;
+    }
+
+    /**
+     * 获取年龄
+     * @return 年龄
+     */
+    public int getOld() {
+	    return old;
+    }
+
+    /**
+     * 描述：设置年龄
+     * @param old 年龄
+     */
+    public void setOld(int old) {
+	    this.old = old;
+    }
+
+    /**
+     * 是否男
+     * @return 是或否
+     */
+    public boolean isMale() {
+	    return sex;
+    }
+
+    /**
+     * 描述：设置性别
+     * @param isMale 是男？
+     */
+    public void setSex(boolean isMale) {
+	    this.sex = isMale;
+    }
+
+    /**
+     * 获取归属地
+     * @return 归属地
+     */
+    public String getHome() {
+	    return home;
+    }
+
+    /**
+     * 设置归属地
+     * @param home 归属地
+     */
+    public void setHome(String home) {
+	    this.home = home;
+    }
 }
